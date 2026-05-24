e88ff7b1935987586b08444394b727f1902b2af6
ammiinka <am.cipova@gmail.com>
2026-05-24 10:09:05 +0200

    First JavaScript calculator project

---
+++ app.js
@@ -0,0 +1,19 @@
+item1 = 10
+item2 = 20
+
+function add (a, b){return a+b}
+function subtract (a,b) {return a-b}
+function divide (a, b) {return a/b}
+function multiply (a,b) {return a*b}
+
+console.log("addiction");
+console.log(add(item1, item2))
+
+console.log ("subtraction")
+console.log(subtract(item1, item2))
+
+console.log ("division")
+console.log(divide(item1, item2))
+
+console.log ("multiplication")
+console.log(multiply(item1, item2))
